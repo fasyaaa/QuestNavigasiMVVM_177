48868158b39c1a370ce35851ddbf0a42ff2cf9db
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-11-21 10:23:38 +0700

    membuat class Halaman

---
+++ app/src/main/java/com/example/mvvmmodel/Navigasi.kt
@@ -0,0 +1,16 @@
+package com.example.mvvmmodel
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.mvvmmodel.ui.viewmodel.MahasiswaViewModel
+
+enum class Halaman{
+    Form,
+    Data
+}
